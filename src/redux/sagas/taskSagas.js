f71b7cb6a156c35e2fa3774cc20253bf30ace866
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-04-16 21:51:37 +0500

    not fixed

--- src/redux/sagas/taskSagas.js
+++ src/redux/sagas/taskSagas.js
@@ -9,7 +9,7 @@ import {
   DELETE_TASK_SUCCESS,
 } from '../actions/actionTypes';
 
-const API_URL = 'http://localhost:3000/tasks'
+const API_URL = 'https://kanban-website.vercel.app'
 
 function* fetchTasks() {
   try {
