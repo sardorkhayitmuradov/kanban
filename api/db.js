b5337fb777986c007008abded352b324782c506d
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-04-16 21:10:09 +0500

    not fixed

---
+++ api/db.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = (req, res) => {
+    const handler = server(req, res);
+    return handler;
+};
