652e957c1ce465df43700ee9f8ea8e9b55dcab06
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-04-16 22:02:57 +0500

    not fixed

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+import { create, router as _router, defaults, rewriter } from 'json-server'
+const server = create()
+const router = _router('db.json')
+const middlewares = defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
+server.use(rewriter({
     '/api/*': '/$1',
     '/tasks/:id': '/:id'
 }))
@@ -16,4 +16,4 @@ server.listen(3000, () => {
 })
 
 // Export the Server API
-module.exports = server
+export default server
